01075eaacc7d9bc81a1676620312f853b7cea0e9
Ted Werbel <werbel.ted@gmail.com>
2017-04-06 13:37:46 -0400

    added api keys for yandex and clarifai

--- src/app.js
+++ src/app.js
@@ -6,16 +6,30 @@ import {
   ListView,
   ActivityIndicator,
   AsyncStorage,
-  Linking
+  Linking,
+  StatusBar
 } from 'react-native';
 import Camera from 'react-native-camera';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Modal from 'react-native-modalbox';
+import Clarifai from 'clarifai'
+
+// API Keys
+import apiKeys from '../src/apiKeys.json';
+const yandexKey = apiKeys.yandex;
+
+// Yandex, for translating concepts
+const yandexGetLang = `https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=${yandexKey}&ui=en`;
+const yandexGetTranslate = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${yandexKey}`;
+
+// Clarifai, for image recognition
+const app = new Clarifai.App(apiKeys.clarifai.id, apiKeys.clarifai.secret);
+
 
 export default class App extends Component {
   render() {
     return (
-      <Text>tESTING 123</Text>
+      <Text>{yandexKey}</Text>
     );
   }
 }
