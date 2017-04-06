7c368d4222ea2f3ca7dcefb1b92f3271cc105b58
Ted Werbel <werbel.ted@gmail.com>
2017-04-06 13:19:37 -0400

    add dependencies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ListView,
+  ActivityIndicator,
+  AsyncStorage,
+  Linking
+} from 'react-native';
+import Camera from 'react-native-camera';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Modal from 'react-native-modalbox';
 
 export default class App extends Component {
   render() {
